32bdf35a580c149a2e276f09f4592ea4746371bc
James Dong <dong.james.sept14@gmail.com>
2016-09-01 11:25:04 -0500

    Simple stuff

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from base64 import urlsafe_b64decode as ub64d, urlsafe_b64encode as ub64e
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
@@ -9,7 +10,8 @@ def index():
 
 @app.route('/do/<urlb64>/get.<ext>')
 def download(urlb64, ext):
-	return ub64d(urlb64)
+	url = ub64d(urlb64).decode('utf-8')
+	return requests.get(url).content
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
